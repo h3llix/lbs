47f8513b6b023131190ca500382c5cd8c356c39f
aryaman-titan <aryamanbehera@gmail.com>
2020-02-21 17:53:25 +0530

    added approve button

--- welcome.php
+++ welcome.php
@@ -1,4 +1,23 @@
 <html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
 <body>
     <h2>
         <a href=/index.php>Logout</a>
