a519d4d1ce8c381904966c43e3e290127598f6c1
aryaman-titan <aryamanbehera@gmail.com>
2020-02-21 17:38:47 +0530

    modified welcome.php

--- welcome.php
+++ welcome.php
@@ -1,7 +1,7 @@
 <html>
 <body>
     <h2>
-        <a href=/index.php>Logut</a>
+        <a href=/index.php>Logout</a>
         
     </h2>
     <?php 
@@ -12,6 +12,39 @@
         header('location:welcome2.php');
     }
     ?>
+
+<?php
+
+
+while($rows=mysqli_fetch_assoc($result))
+{
+    ?>
+
+ <tr>
+    <td><?php echo $rows['bookid']; ?></td>
+    <td><?php echo $rows['bookname']; ?></td>
+    <td><?php echo $rows['bookissued']; ?></td>
+    <td><?php echo $rows['bookleft']; ?></td>
+  </tr>
+
+<?php
+}
+?>
+  </table>
+  <form action="insert.php" method="post">
+    <p>
+        <label >Book Name:</label>
+        <input type="text" name="bookname" id="firstName">
+    </p>
+    <p>
+        <label>Book Issued:</label>
+        <input type="text" name="bookissued" id="lastName">
+    </p>
+    <p>
+        <label >Book Left</label>
+        <input type="text" name="bookleft" id="emailAddress">
+    </p>
+    <input type="submit" value="Submit">
 </body>
 
 
